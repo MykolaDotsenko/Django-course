c3a2991610c7cc3b26b3485eaa677a97f5889155
Mykola Dotsenko <docnikolaj1990@gmail.com>
2025-05-22 20:56:15 +0300

    first migration

---
+++ django-blog/apps/post/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+
+# Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+        ("archived", "Archived")
+    )
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, blank=True)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['-created_at', 'title']
+    def __str__ (self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
--- django-blog/config/settings.py
+++ django-blog/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #development
+    'apps.post', 
 ]
 
 MIDDLEWARE = [
