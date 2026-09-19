a28ebbabb5ce991cdffbe853072b8c2ff509dc57
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 16:30:40 +0300

    fix: enforce DEBUG-only media preview in view

--- django-blog/apps/common/views.py
+++ django-blog/apps/common/views.py
@@ -1,10 +1,15 @@
+from django.conf import settings
+from django.http import Http404
 from django.shortcuts import render
 
 from .presentation.media_preview import build_media_preview_context
 
 
 def media_preview(request):
-    """Render the DEBUG-only Quiet Atlas media QA surface."""
+    """Render the Quiet Atlas QA surface only when DEBUG is enabled."""
+
+    if not settings.DEBUG:
+        raise Http404
 
     return render(
         request,
