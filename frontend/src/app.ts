e124d4ad974efeebc301dc2a5951fcbb8d844413
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-20 22:24:56 +0300

    PR7.3: wire lazy chart loader into progressive frontend

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -2,4 +2,5 @@ import "@fontsource-variable/inter/wght.css";
 import "htmx.org";
 import "./behaviors/current-converter";
 import "./behaviors/picker";
+import "./behaviors/rate-chart-loader";
 import "./styles/app.css";
