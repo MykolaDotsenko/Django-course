7e7feb24c10091332fd50bad48cba8c3ee1b892b
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-22 11:10:50 +0300

    integrate PR11: update frontend/src/app.ts

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,6 +1,7 @@
 import "@fontsource-variable/inter/wght.css";
 import "htmx.org";
 import "./behaviors/current-converter";
+import "./behaviors/local-saved-state";
 import "./behaviors/picker";
 import "./behaviors/rate-chart-loader";
 import "./styles/app.css";
